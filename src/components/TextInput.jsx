e41f38b6de432f14d4dee5e65144f0d530278207
Colin Bate <colin@colinbate.com>
2015-12-29 22:25:58 -0400

    Fix #1. Do dirty check when TextInput updated.

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 
 class TextInput extends Component {
+  componentDidUpdate() {
+    if (this.widget.MaterialTextfield && this.props.value) {
+      this.widget.MaterialTextfield.checkDirty();
+    }
+  }
   render() {
     let {id, label, type, value, onChange, onBlur, onFocus} = this.props;
     return (
-      <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" ref={(ref) => this.widget = ref}>
         <input className="mdl-textfield__input" type={type || 'text'} id={id} value={value} onChange={onChange} onBlur={onBlur} onFocus={onFocus} ref={(ref) => this.input = ref} />
         <label className="mdl-textfield__label" htmlFor={id}>{label}</label>
       </div>
