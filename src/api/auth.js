4215b3d54ed7a5d0e444b41c111c912ed9bd6a5c
Colin Bate <colin@colinbate.com>
2017-01-05 22:11:40 -0400

    Update to Auth0 Lock 10

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,23 +1,43 @@
 import storage from 'storage';
 export const tokenKey = 'ksr-auth-token';
 export const profileKey = 'ksr-auth-profile';
-const mylock = new window.Auth0Lock('OpXoUvqthD5rhxm6hUXHo8x3fppFbeMt', 'colinbate.auth0.com');
+const mylock = new window.Auth0Lock(
+  'OpXoUvqthD5rhxm6hUXHo8x3fppFbeMt',
+  'colinbate.auth0.com',
+  {
+    auth: {
+      redirect: false,
+      params: {scope: 'openid admin'}
+    }
+  }
+);
+
+let showResolve;
+let showReject;
+mylock.on('authenticated', result => {
+  if (!showResolve || !showReject) {
+    return;
+  }
+  mylock.getProfile(result.idToken, function(error, profile) {
+    if (error) {
+      return showReject(error);
+    }
+    if (result.idToken) {
+      storage.set(tokenKey, result.idToken);
+    }
+    if (profile) {
+      storage.set(profileKey, profile);
+    }
+    showResolve(profile);
+  });
+});
 
 
 export function signin() {
   return new Promise((resolve, rej) => {
-    mylock.show({authParams: {scope: 'openid admin'}}, (err, profile, token) => {
-      if (err) {
-        return rej(err);
-      }
-      if (token) {
-        storage.set(tokenKey, token);
-      }
-      if (profile) {
-        storage.set(profileKey, profile);
-      }
-      resolve(profile);
-    });
+    showReject = rej;
+    showResolve = resolve;
+    mylock.show();
   });
 }
 
